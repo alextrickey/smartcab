0ac233ae34fc9bb0ccb5f86b963b1d1d66b917d3
Alex Trickey <k.alex.trickey@gmail.com>
2016-12-07 21:15:51 -0800

    feat: Find best constant alpha with linear epsilon.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -19,13 +19,11 @@ class LearningAgent(Agent):
         self.epsilon = epsilon   # Random exploration factor
         self.alpha = alpha       # Learning factor
 
-        ###########
-        ## TO DO ##
-        ###########
         # Set any additional class parameters as needed
+        self.trial = 0
 
 
-    def reset(self, destination=None, testing=False):
+    def reset(self, destination=None, testing=False, trial = 0):
         """ The reset function is called at the beginning of each trial.
             'testing' is set to True if testing trials are being used
             once training trials have completed. """
@@ -36,7 +34,11 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-        self.epsilon = self.epsilon - 0.05 #self.epsilon = 1.0/(self.trial**2)
+        #self.trial += 1
+        #self.epsilon = self.alpha**self.trial
+        self.epsilon = self.epsilon - 0.05 
+        #self.epsilon = 1.0/(self.trial**2)
+
         if testing == True:
             self.alpha = 0
             self.epsilon = 0
@@ -172,7 +174,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent,learning=True)
+    agent = env.create_agent(LearningAgent,learning=True,alpha=0.9)
     
     ##############
     # Follow the driving agent
@@ -187,14 +189,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,update_delay=0.01,display=False,log_metrics=True)
+    sim = Simulator(env,update_delay=0.01,display=False,log_metrics=True,optimized=True)
 
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=100)
 
 
 if __name__ == '__main__':
